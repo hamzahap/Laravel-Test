c5f0d89e10f0331fd1b290e34ae18ec8d0bb2502
Hamzah Punjabi <hamzahqatar123@gmail.com>
2021-10-13 11:36:29 -0230

    Add Factory

---
+++ Hking/database/factories/HikingFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Hiking;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class HikingFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Hiking::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
