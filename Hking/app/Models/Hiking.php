b0bcecfa8ae03354319b0047b72696fd306c946c
Hamzah Punjabi <hamzahqatar123@gmail.com>
2021-10-13 11:28:25 -0230

    Make Model

---
+++ Hking/app/Models/Hiking.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Hiking extends Model
+{
+    use HasFactory;
+}
